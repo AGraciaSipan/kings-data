74739ec1884e072e99b33542bd68d6966b0e7533
Alvaro <gracia.sipan.alvaro@gmail.com>
2024-11-17 23:31:06 +0100

    feat: adding competition formats

--- src/enums/__init__.py
+++ src/enums/__init__.py
@@ -1,4 +1,5 @@
+from .competition_format import CompetitionFormat
 from .kingdom import Kingdom
 from .region import Region
 
-__all__ = ["Kingdom", "Region"]
+__all__ = ["CompetitionFormat", "Kingdom", "Region"]
---
+++ src/enums/competition_format.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class CompetitionFormat(Enum):
+    CUP = "cup"
+    LEAGUE = "league"
